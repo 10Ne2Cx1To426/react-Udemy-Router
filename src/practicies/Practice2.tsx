a1cc799a2d3ce4e6d33b3df44b9385f69db142c7
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-04-20 23:40:41 +0900

    c

--- src/practicies/Practice2.tsx
+++ src/practicies/Practice2.tsx
@@ -1,5 +1,11 @@
 export const Practice2 = () => {
-  const onClickPractice = () => alert();
+  const getTotalFee = (num: number): number => {
+    const total = num * 1.1;
+    return total;
+  };
+  const onClickPractice = () => {
+    console.log(getTotalFee(1000));
+  };
 
   return (
     <div>
